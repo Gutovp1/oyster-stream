3a0e56abfb46d7ee8a845da9870b94c1f805cb33
gutovp1 <paivaav@gmail.com>
2024-03-22 20:25:48 -0300

    Create a separate file per entitty

---
+++ entities/Lantern.ts
@@ -0,0 +1,26 @@
+export enum OysterType {
+  Baby = "baby",
+  Medium = "medium",
+  Large = "large",
+}
+
+export enum LanternType {
+  WhiteNursery = "white-nursery",
+  BlackNursery = "black-nursery",
+  Chilean = "chilean",
+  Intermediate = "intermediate",
+  BabyStock = "baby-stock",
+  MediumStock = "medium-stock",
+  LargeStock = "large-stock",
+}
+
+export type Lantern = {
+  //Belong to longline
+  position_at_longline: number[];
+  lantern_type: LanternType;
+  oyster_type: OysterType;
+  level_quantity: number;
+  oyster_quantity: number[];
+  in_sea_started_at: Date;
+  expected_maturation_at: Date;
+};
