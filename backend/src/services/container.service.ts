3174c6bf4ea728342b24b69743568f5dfba27d90
gutovp1 <paivaav@gmail.com>
2024-04-05 22:29:40 -0300

    create controller route paths and services for container

--- backend/src/services/container.service.ts
+++ backend/src/services/container.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Container } from 'src/entities/container';
+import { Container, OysterType } from 'src/entities/container';
 
 @Injectable()
 export class ContainerService {
@@ -23,5 +23,29 @@ export class ContainerService {
 
   async findOne(containerId: number): Promise<Container> {
     return this.containerRepository.findOne({ where: { id: containerId } });
+  }
+
+  // must ensure that "expected_maturation_at" does not contain a time component, but only date
+  async findAllToday(): Promise<Container[]> {
+    const today = new Date(); // 2024-04-05T00:00:00.000Z
+    const formattedDate = today.toISOString().split('T')[0];
+
+    const convertedDate = new Date(formattedDate);
+
+    return this.containerRepository.find({
+      where: { expected_maturation_at: convertedDate },
+    });
+  }
+
+  async findAllOystersToMature(
+    type: OysterType,
+  ): Promise<{ oyster_quantity: number[]; expected_maturation_at: Date }[]> {
+    const containers = await this.containerRepository.find({
+      where: { oyster_type: type },
+    });
+    return containers.map((container) => ({
+      oyster_quantity: container.oyster_quantity,
+      expected_maturation_at: container.expected_maturation_at,
+    }));
   }
 }
